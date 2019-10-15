6f2a93b9bd25f81bc96e08a43ef8feff0c6754c8
FoxLisik <40771029+FoxLisik@users.noreply.github.com>
2019-10-15 21:58:41 +0400

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -10,6 +10,16 @@ Bot = commands.Bot(command_prefix= "!s")
 async def info(ctx):
     author = ctx.message.author
     await ctx.send("Этот бот в разроботке! Вы можете предложить свои идеи и высказать недовольство тут: https://discord.gg/ykdtjYY")
+    
+@Bot.command()
+async def USE CODE(ctx):
+    author = ctx.message.author
+    await ctx.send("SFZD")
+
+@Bot.command()
+async def Артем(ctx):
+    author = ctx.message.author
+    await ctx.send("ЕБЛАН БЛЯТЬ КОНЧЕНЫЙ")
 
 token = os.environ.get('BOT_TOKEN')
 Bot.run(str(token))
