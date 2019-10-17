698bf3ed92fc09d939c352c7ec70c5009f4c8da2
FoxLisik <40771029+FoxLisik@users.noreply.github.com>
2019-10-17 20:41:27 +0400

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -4,7 +4,7 @@ import os
 from discord.ext import commands
 from discord.ext.commands import Bot
 
-Bot = commands.Bot(command_prefix= "!s")
+Bot = commands.Bot(command_prefix= "!s ")
 
 @Bot.command()
 async def info(ctx):
