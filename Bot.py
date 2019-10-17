cdf1908fdd207040196592b974fb69e1f54e836e
FoxLisik <40771029+FoxLisik@users.noreply.github.com>
2019-10-17 21:48:35 +0400

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -6,14 +6,16 @@ from discord.ext.commands import Bot
 
 Bot = commands.Bot(command_prefix= "!s ")
 
-@Bot.event()
-async def on_ready():
-    print('дарова ептый')
-
 @Bot.command()
 async def info(ctx):
     author = ctx.message.author
     await ctx.send("Этот бот в разроботке! Вы можете предложить свои идеи и высказать недовольство тут: https://discord.gg/ykdtjYY")
 
+@Bot.command()
+@commands.has_permissions(administrator= True)
+async def mute(ctx, member: discord.Member):
+    mute_role = discord.utils.get(ctx.message.guild.roles, name= 'mute')
+    await member.add_roles(mute_role)
+    
 token = os.environ.get('BOT_TOKEN')
 Bot.run(str(token))
