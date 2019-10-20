37aa758616519e572cb86dbe0aa5f52077a2eaa8
FoxLisik <40771029+FoxLisik@users.noreply.github.com>
2019-10-20 17:15:17 +0400

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -7,18 +7,24 @@ from discord.ext.commands import Bot
 Bot = commands.Bot(command_prefix= "s!")
 
 
-mute_role = discord.utils.get(ctx.message.guild.roles, name= 'таймач😫')
-
 
 @Bot.command()
 async def info(ctx):
     author = ctx.message.author
     await ctx.send("Этот бот в разроботке! Вы можете предложить свои идеи и высказать недовольство тут: https://discord.gg/ykdtjYY")
+
     
-@Bot.command()
-@commands.has_permissions(administrator= True)
+@bot.command(pass_context = True)
 async def mute(ctx, member: discord.Member):
-    await member.add_roles(mute_role)
+     if ctx.message.author.server_permissions.administrator or ctx.message.author.id == '194151340090327041':
+        role = discord.utils.get(member.server.roles, name='таймач😫')
+        await bot.add_roles(member, role)
+        embed=discord.Embed(title="User Muted!", description="**{0}** was muted by **{1}**!".format(member, ctx.message.author), color=0xff00f6)
+        await bot.say(embed=embed)
+     else:
+        embed=discord.Embed(title="Permission Denied.", description="You don't have permission to use this command.", color=0xff00f6)
+        await bot.say(embed=embed)
+
     
 
 token = os.environ.get('BOT_TOKEN')
