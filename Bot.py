d7e107fe25ef2ab64e7c5407ae4ce0c12010c7db
FoxLisik <40771029+FoxLisik@users.noreply.github.com>
2019-10-20 17:18:47 +0400

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -16,8 +16,8 @@ async def info(ctx):
     
 @bot.command(pass_context = True)
 async def mute(ctx, member: discord.Member):
-     if ctx.message.author.server_permissions.administrator or ctx.message.author.id == '194151340090327041':
-        role = discord.utils.get(member.server.roles, name='таймач😫')
+     if ctx.message.author.Permissions.manage_guild.administrator:
+        role = discord.utils.get(member.guild.roles, name='таймач😫')
         await bot.add_roles(member, role)
         embed=discord.Embed(title="User Muted!", description="**{0}** was muted by **{1}**!".format(member, ctx.message.author), color=0xff00f6)
         await bot.say(embed=embed)
