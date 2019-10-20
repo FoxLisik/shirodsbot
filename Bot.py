2385e99df050ddaba310da39264b72f532cc8cbd
FoxLisik <40771029+FoxLisik@users.noreply.github.com>
2019-10-20 16:57:57 +0400

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -4,15 +4,12 @@ import os
 from discord.ext import commands
 from discord.ext.commands import Bot
 
-Bot = commands.Bot(command_prefix= "!s ")
+Bot = commands.Bot(command_prefix= "s!")
 
 @Bot.command()
 async def info(ctx):
     author = ctx.message.author
     await ctx.send("Этот бот в разроботке! Вы можете предложить свои идеи и высказать недовольство тут: https://discord.gg/ykdtjYY")
-
-@Bot.command()
-@commands.has_permissions(administrator= True)
     
 @Bot.command()
 @commands.has_permissions(administrator= True)
